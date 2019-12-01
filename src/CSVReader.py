e45a4893b2ef2d25ae60abad7df425d49585fad5
Christine002 <56103318+Christine002@users.noreply.github.com>
2019-12-01 12:39:17 -0500

    Fix: Update csvreader

--- src/CSVReader.py
+++ src/CSVReader.py
@@ -1,4 +1,5 @@
 import csv
+from Fileutilities.absolutepath import absolutepath
 
 
 def ClassFactory(class_name, dictionary):
@@ -9,8 +10,16 @@ class CsvReader:
     data = []
 
     def __init__(self, filepath):
-        self.data.clear()
-        with open(filepath) as text_data:
+        self.data = []
+
+        with open(absolutepath(filepath)) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
+        pass
+
+    def return_data_as_objects(self, class_name):
+        objects = []
+        for row in self.data:
+            objects.append(ClassFactory(class_name, row))
+        return objects
